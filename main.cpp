9f66cc055429f39ee77dc467516c42b11f924f19
grigory karpunin <143996987+sae1012@users.noreply.github.com>
2024-04-15 13:08:24 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+string task1(int number) {
+    vector <string> days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    if (number >= 1 && number <= 7)
+    {
+        return days[number - 1];
+    }
+    else
+    {
+        return "Error";
+    }
+}
+
+
+string task2(int number) {
+    vector <string> month = {"January", "February", "March", "April", "May", "June", "Jule", "August", "September", "October", "November", "December"};
+    if (number >= 1 && number <= 12)
+    {
+        return month[number - 1];
+    }
+    else
+    {
+        return "Error";
+    }
+}
+
+
+string task3(int number) {
+    vector <string> season = {"Winter", "Spring", "Summer", "Autumn"};
+    
+    if (number == 12 || number == 1 || number == 2)
+    {
+        return season[0];
+    }
+    if (number == 3 || number == 4 || number == 5)
+    {
+        return season[1];
+    }
+    if (number == 6 || number == 7 || number == 8)
+    {
+        return season[2];
+    }
+    if (number == 9 || number == 10 || number == 11)
+    {
+        return season[3];
+    }
+    if (number <= 0 || number > 12)
+    {
+        return "Error";
+    }
+}
+
+
+int task4(int number) {
+    string check;
+    cout << "Is this a leap year? Answer: Yes/No " << "Enter answer:";
+    cin >> check;
+    
+    if (number == 2 && check == "Yes")
+    {
+        return 29;
+    }
+    else if (number == 2 && check == "No")
+    {
+        return 28;
+    }
+    else if (number == 4 || number == 6 || number == 9 || number == 11)
+    {
+        return 30;
+    }
+    else if (number == 1 || number == 3 || number == 5 || number == 7 || number == 8 || number == 10 || number == 12)
+    {
+        return 31;
+    }
+    
+    
+int task5(int number){
+    if (number == 4 || number == 6 || number == 9 || number == 11)
+    {
+        return 30;
+    }
+    else if (number == 1 || number == 3 || number == 5 || number == 7 || number == 8 || number == 10 || number == 12)
+    {
+        return 31;
+    }
+    else
+    {
+        return 28;
+    }
+}
+
+
+int main() {
+    int number;
+    cout << "Enter number:" << endl;
+    cin >> number;
+    cout << task5(number) << endl;
+}
